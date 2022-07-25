caa2eec08dcc89ca2db98ae2a9c6f83009bb9290
Maxim <max.sherb2007@gmail.com>
2022-07-25 13:28:03 +0300

    Initial commit

---
+++ src/test/java/com/max/projectmanager/ProjectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
