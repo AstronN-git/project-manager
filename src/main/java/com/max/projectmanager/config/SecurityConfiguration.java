f9cc98c907b3f89ee092b40e9f59122a80247ddb
Maxim <max.sherb2007@gmail.com>
2022-08-03 12:19:02 +0300

    Started with Spring Security

---
+++ src/main/java/com/max/projectmanager/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.max.projectmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
