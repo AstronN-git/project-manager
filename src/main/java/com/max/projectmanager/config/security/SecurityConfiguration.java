2f34b178af13140a1bc14115fcd7b00c8a8592b3
Maxim <max.sherb2007@gmail.com>
2022-08-18 01:04:52 +0300

    Login form

--- src/main/java/com/max/projectmanager/config/security/SecurityConfiguration.java
+++ src/main/java/com/max/projectmanager/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.max.projectmanager.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,9 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+    @Value("${url.todo}")
+    private String todoUrl;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.
@@ -16,8 +20,9 @@ public class SecurityConfiguration {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .logout(configurer -> configurer.logoutUrl("/logout"))
                 .formLogin()
+                .loginPage("/login")
+                .permitAll()
                 .and()
                 .httpBasic();
 
